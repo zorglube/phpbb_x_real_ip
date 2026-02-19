21e582f0b3c2af85f719edb64ba6925cfbd75061
Zorglube <630192+zorglube@users.noreply.github.com>
2026-02-19 15:16:06 +0100

    Fix language entries in common.php for French

--- brctx/xrealip/language/fr/common.php
+++ brctx/xrealip/language/fr/common.php
@@ -18,8 +18,8 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-    'ACP_REVERSE_PROXY'        => 'Reverse Proxy IP',
-    'ACP_REVERSE_PROXY_NOTICE' => '<div class="phpinfo"><p>Il n\'y a pas de paramètre pour cette extention. L\'adresse IP sera normalisé et associé à la session utilisteur. Enjoy!<br />Vous risquez d\'être déconnecté après l\'activation de l\'extention. C\'est tout à fait normal, puisque du point de vue du \"board\" votre IP va passer de celle du \"reverse proxy\" à votre IP réelle. Identifiez vous à nouveau.</p></div>',
-    'ACP_XREALIP_LEGEND'					=> 'X Real IP',
-	'ACP_ACP_XREALIP_HEADER_NAME'			=> 'Name of the header that carry the IP.',
+    'ACP_REVERSE_PROXY'				=> 'Reverse Proxy IP',
+    'ACP_REVERSE_PROXY_NOTICE'		=> '<div class="phpinfo"><p>Il n\'y a pas de paramètre pour cette extention. L\'adresse IP sera normalisé et associé à la session utilisteur. Enjoy!<br />Vous risquez d\'être déconnecté après l\'activation de l\'extention. C\'est tout à fait normal, puisque du point de vue du \"board\" votre IP va passer de celle du \"reverse proxy\" à votre IP réelle. Identifiez vous à nouveau.</p></div>',
+    'ACP_XREALIP_LEGEND'			=> 'X Real IP',
+	'ACP_ACP_XREALIP_HEADER_NAME'	=> 'Name of the header that carry the IP.',
 ));
